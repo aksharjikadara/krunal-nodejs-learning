1b44d6189b0c9e80b186d0b75652c7908ece9bf5
Akshar Jikadara <jikadara2aj@gmail.com>
2023-09-03 10:31:28 +0530

    create http server

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+// console.log('hello world');
+require('dotenv').config()
+const http = require('http')
+
+const PORT = process.env.PORT
+
+const server = http.createServer((req, res) => {
+  if (req.url == '/') {
+    res.end('hello nodejs')
+  } else if (req.url == '/login') {
+    res.end('<h1>You are in login page</h1>')
+  }
+})
+
+server.listen(PORT, () => {
+  console.log(`server is started at http://localhost:${PORT}`);
+})
