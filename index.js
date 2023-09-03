7e045eb2138fe634bd7167ebdecc45922a5c1108
Akshar Jikadara <jikadara2aj@gmail.com>
2023-09-03 11:13:50 +0530

    add fs modules create html file

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 // console.log('hello world');
 require('dotenv').config()
 const http = require('http')
+const fs = require('fs')
 
 const PORT = process.env.PORT
+
+const filePathByAsync = fs.readFile('index.html', 'utf-8' , (error, data) => {
+  // console.log('filePathByAsync', data);
+})
+const filePathBySync = fs.readFileSync('index.html', 'utf-8')
+// console.log('filePathBySync', filePathBySync);
 
 const server = http.createServer((req, res) => {
   if (req.url == '/') {
     res.end('hello nodejs')
   } else if (req.url == '/login') {
     res.end('<h1>You are in login page</h1>')
+  } else if (req.url == '/html') {
+    res.setHeader('Content-type', 'text/html')
+    res.end(filePathBySync);
+  } else {
+    res.end('<h1>404 not found</h1>')
   }
 })
 
