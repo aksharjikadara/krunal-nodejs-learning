250f0c1bbc06a44ac4cb9026233c0a1c788fcb8c
Akshar Jikadara <jikadara2aj@gmail.com>
2023-09-10 16:34:45 +0530

    add getbyid routes

--- src/controller/user.js
+++ src/controller/user.js
@@ -7,13 +7,12 @@ const data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
 const users = data.users
 
 const createUser = (req, res) => {
-  // console.log(req);
   const user = req.body;
   users.push(user);
   const writeData = {
-    "users" : users
+    "users": users
   }
-  const writeFile =fs.writeFileSync(dataPath, JSON.stringify(writeData))
+  const writeFile = fs.writeFileSync(dataPath, JSON.stringify(writeData))
   res.status(200).send(user)
 }
 
@@ -21,7 +20,19 @@ const getAllUsers = (req, res) => {
   res.status(200).send(users)
 }
 
+const getById = (req, res) => {
+  const id = req.params.id
+  const data = users.find((user) => {
+    return user.id === id
+  })
+  if (!data) {
+    res.status(404).json({ message: 'User Not Found' })
+  }
+  res.status(200).send(data)
+}
+
 module.exports = {
   createUser,
-  getAllUsers
+  getAllUsers,
+  getById
 };
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 router = express.Router();
 
-const { createUser, getAllUsers} = require('../controller/user')
+const { createUser, getAllUsers, getById } = require('../controller/user')
 
 router.post('/user/create', createUser)
 router.get('/user/getAll', getAllUsers)
+router.get('/user/get/:id', getById)
 
 module.exports = router
